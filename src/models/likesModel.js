09bd3ed9e87c9c74f030368a818eb9ad81bdfbc3
sethLundst <sethlundstram@protonmail.com>
2022-06-22 23:47:42 +0100

    Debug

--- src/models/likesModel.js
+++ src/models/likesModel.js
@@ -12,10 +12,6 @@ exports.selectLikesByUser = async (username) => {
 };
 
 exports.insertLikeByUser = async (username, article_id, like_dislike) => {
-  await db.query(`DELETE FROM likes WHERE username = $1 AND article_id = $2;`, [
-    username,
-    article_id,
-  ]);
   const result = await db.query(
     `INSERT INTO likes (username, article_id, like_dislike) 
     VALUES ($1, $2, $3) RETURNING *;`,
@@ -29,12 +25,6 @@ exports.removeLikeByUser = async (username, article_id) => {
     username,
     article_id,
   ]);
-  const result2 = await db.query(
-    format(
-      `SELECT article_id, like_dislike FROM likes WHERE username = %L;`,
-      username
-    )
-  );
   const result = await db.query(
     `SELECT FROM likes WHERE username = $1 AND article_id = $2;`,
     [username, article_id]
